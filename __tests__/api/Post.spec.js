7ddf147363653207ef13de23c48bc816cbc4ff94
AsbarAli <loons.create@gmail.com>
2019-08-25 11:57:05 +0530

    Add Test for post service

--- __tests__/api/Post.spec.js
+++ __tests__/api/Post.spec.js
@@ -1,9 +1,40 @@
 import PostService from '../../src/api/PostService';
+import {setBaseURL} from '../../src/api/RestService';
+
+setBaseURL('http://baseURL');
 
 describe('test `PostService`', () => {
-  it('Check happ path', () => {
-    return PostService.getAllPosts().then((res) => {
-      expect(res).toBe('ss');
+  it('Fetches post data from server when server returns a successful response', async () => {
+    const mockSuccessResponse = [
+      {
+        userId: 1,
+        id: 1,
+        title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+        body: 'quia et suscipit',
+      },
+    ];
+
+    // Since we await the call to getAllPosts() in PostService.js,
+    // we Promise.resolve it in the test to a mockSuccessResponse object.
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
+
+    // Since we are awaiting the call to fetch in Rest Service and expecting the returned object to contain a json function,
+    // which returns another Promise, we Promise.resolve to an object containing such a json function.
+    const mockFetchPromise = Promise.resolve({// 3
+      json: () => mockJsonPromise,
     });
+
+    // Spy on global.fetch and mock it’s implementation to return the Promise object
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+    // Since we await the call to getAllPosts() in PostService.js,
+    // we Promise.resolve it in the test to a mockSuccessResponse object.
+    const response = await PostService.getAllPosts();
+    expect(response[0].title).toBe('sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
+    expect(response[0].body).toBe('quia et suscipit');
+
+    // Clear the mock
+    global.fetch.mockClear();
+    delete global.fetch;
   });
 });
