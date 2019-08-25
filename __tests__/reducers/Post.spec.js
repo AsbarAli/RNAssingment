b84910b65241410f6ce29bb6c9249586c25a36d0
AsbarAli <loons.create@gmail.com>
2019-08-25 16:55:58 +0530

    Update the test reducer

--- __tests__/reducers/Post.spec.js
+++ __tests__/reducers/Post.spec.js
@@ -2,15 +2,8 @@ import {GET_POSTS} from '../../src/shared/actions/Types';
 import PostReducer from '../../src/module/reducers/PostReducer';
 
 describe('Post reducer', () => {
-  const expectedInitialState = {
-    postList: null,
-    getPostListLoading: false,
-    getPostListError: null,
-
-    photoList: null,
-    getAlbumListLoading: false,
-    getAlbumListError: null,
-  };
+  const expectedInitialState = {postList: null, getPostListLoading: false, getPostListError: null,
+    photoList: null, getAlbumListLoading: false, getAlbumListError: null};
 
   it('Should return default state', () => {
     const newState = PostReducer(expectedInitialState, {});
@@ -18,15 +11,8 @@ describe('Post reducer', () => {
   });
 
   it('Should return new state if receiving type', () => {
-    const expectedOutput = {
-      postList: null,
-      getPostListLoading: true,
-      getPostListError: null,
-
-      photoList: null,
-      getAlbumListLoading: false,
-      getAlbumListError: null,
-    };
+    const expectedOutput = {postList: null, getPostListLoading: true, getPostListError: null,
+      photoList: null, getAlbumListLoading: false, getAlbumListError: null};
 
     const newState = PostReducer(expectedInitialState, {
       type: GET_POSTS,
